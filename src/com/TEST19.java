11e94d2a5279401c5d6d0318ce19c1ed7a651b7e
Vanshika0467 <vanshikapandey1406@gmail.com>
2025-06-06 18:56:16 +0530

    Second

--- src/com/TEST19.java
+++ src/com/TEST19.java
@@ -1,5 +1,11 @@
 package com;
 
 public class TEST19 {
+	public static void main(String[] args) {
+		
+		
+		System.out.println("11111111111111111111");
+		System.out.println("22222222222222222222");
+	}
 
 }
